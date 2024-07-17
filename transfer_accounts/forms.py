1475026aec000d209a8c5db731ca2d98b7a521ce
AhmedOsama93 <a.osama9392@gmail.com>
2024-07-17 19:09:57 +0300

    edit transfer view and success message

--- transfer_accounts/forms.py
+++ transfer_accounts/forms.py
@@ -4,6 +4,6 @@ class ImportAccountsForm(forms.Form):
     file = forms.FileField()
 
 class TransferForm(forms.Form):
-    from_account_id = forms.IntegerField()
-    to_account_id = forms.IntegerField()
+    from_account_id = forms.CharField()
+    to_account_id = forms.CharField()
     amount = forms.DecimalField(max_digits=10, decimal_places=2)
